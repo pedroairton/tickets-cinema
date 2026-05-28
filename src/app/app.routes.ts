1b58d9c3325eaf619ca96fac41ca1aa93c26f050
pedroairtonz <pedroairton@zaite.com.br>
2026-05-28 16:54:54 -0300

    Página de edição/criação de filme criada, form em construção

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,75 +3,92 @@ import { authGuard } from './core/guards/auth-guard';
 import { adminGuard } from './core/guards/admin-guard';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        loadComponent: () => import('./features/user/login/login').then(m => m.Login)
-    },
-    {
+  {
+    path: 'login',
+    loadComponent: () => import('./features/user/login/login').then((m) => m.Login),
+  },
+  {
+    path: '',
+    children: [
+      {
         path: '',
-        children: [
-            {
-                path:'', redirectTo: 'home', pathMatch: 'full'
-            },
-            {
-                path: 'home',
-                loadComponent: () => import('./features/user/home/home').then(m => m.Home)
-            },
-            {
-                path: 'filmes',
-                loadComponent: () => import('./features/user/movies/movies').then(m => m.Movies)
-            },
-            {
-                path: 'filmes/:slug',
-                loadComponent: () => import('./features/user/movie-detail/movie-detail').then(m => m.MovieDetail)
-            },
-            {
-                path: 'historico',
-                canActivate: [authGuard],
-                loadComponent: () => import('./features/user/history/history').then(m => m.History)
-            },
-            {
-                path: 'sessoes/:id',
-                loadComponent: () => import ('./features/user/screening/screening-detail/screening-detail').then(m => m.ScreeningDetail)
-            },
-        ]
-    },
-    {
-        path: 'admin',
-        children: [
-            {
-                path: 'login',
-                loadComponent: () => import('./features/admin/login/login').then(m => m.Login)
-            },
-            {
-                path: 'dashboard',
-                canActivate: [adminGuard],
-                loadComponent: () => import('./features/admin/dashboard/dashboard.component').then(m => m.DashboardComponent)
-            },
-            {
-                path: 'filmes',
-                canActivate: [adminGuard],
-                loadComponent: () => import('./features/admin/movies/movies').then(m => m.Movies)
-            },
-            {
-                path: 'filmes/:id',
-                canActivate: [adminGuard],
-                loadComponent: () => import('./features/admin/movie-detail/movie-detail').then(m => m.MovieDetail)
-            },
-            {
-                path: 'sessoes',
-                canActivate: [adminGuard],
-                loadComponent: () => import('./features/admin/screenings/screenings').then(m => m.Screenings)
-            },
-            {
-                path: 'salas',
-                canActivate: [adminGuard],
-                loadComponent: () => import('./features/admin/rooms/rooms').then(m => m.Rooms)
-            }
-        ]
-    },
-    {
-        path: '**',
-        redirectTo: 'home'
-    }
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () => import('./features/user/home/home').then((m) => m.Home),
+      },
+      {
+        path: 'filmes',
+        loadComponent: () => import('./features/user/movies/movies').then((m) => m.Movies),
+      },
+      {
+        path: 'filmes/:slug',
+        loadComponent: () =>
+          import('./features/user/movie-detail/movie-detail').then((m) => m.MovieDetail),
+      },
+      {
+        path: 'historico',
+        canActivate: [authGuard],
+        loadComponent: () => import('./features/user/history/history').then((m) => m.History),
+      },
+      {
+        path: 'sessoes/:id',
+        loadComponent: () =>
+          import('./features/user/screening/screening-detail/screening-detail').then(
+            (m) => m.ScreeningDetail,
+          ),
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./features/admin/login/login').then((m) => m.Login),
+      },
+      {
+        path: 'dashboard',
+        canActivate: [adminGuard],
+        loadComponent: () =>
+          import('./features/admin/dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent,
+          ),
+      },
+      {
+        path: 'filmes',
+        canActivate: [adminGuard],
+        loadComponent: () => import('./features/admin/movies/movies').then((m) => m.Movies),
+      },
+      {
+        path: 'filmes/editar/:id',
+        canActivate: [adminGuard],
+        loadComponent: () =>
+          import('./features/admin/movie-detail/movie-detail').then((m) => m.MovieDetail),
+      },
+      {
+        path: 'filmes/novo',
+        canActivate: [adminGuard],
+        loadComponent: () =>
+          import('./features/admin/movie-detail/movie-detail').then((m) => m.MovieDetail),
+      },
+      {
+        path: 'sessoes',
+        canActivate: [adminGuard],
+        loadComponent: () =>
+          import('./features/admin/screenings/screenings').then((m) => m.Screenings),
+      },
+      {
+        path: 'salas',
+        canActivate: [adminGuard],
+        loadComponent: () => import('./features/admin/rooms/rooms').then((m) => m.Rooms),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
 ];
--- src/app/features/admin/movie-detail/movie-detail.ts
+++ src/app/features/admin/movie-detail/movie-detail.ts
@@ -3,24 +3,40 @@ import { ActivatedRoute } from '@angular/router';
 import { AdminService } from '../../../core/services/admin.service';
 import { ToastrService } from 'ngx-toastr';
 import { Movie } from '../../../core/models/movie.model';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-detail',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSelectModule, MatRadioModule],
   templateUrl: './movie-detail.html',
   styleUrl: './movie-detail.scss',
 })
 export class MovieDetail {
-  movieId: number
+  isEditMode = signal(false)
+  movieId?: number
   movie = signal<Movie | null>(null)
   private adminService = inject(AdminService)
   private toastr = inject(ToastrService)
+  movieForm = {}
 
   constructor(private route: ActivatedRoute) {
     this.movieId = this.route.snapshot.params['id'];
+    this.isEditMode.set(!!this.movieId);
+    console.log(this.movieId);
+  }
+
+  ngOnInit() {
+    this.loadMovie()
   }
 
   loadMovie(){
+    if(!this.movieId) return
     this.adminService.getMovieById(this.movieId).subscribe({
       next: (res) => {
         console.log(res);
