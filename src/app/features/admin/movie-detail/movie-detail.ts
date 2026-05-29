4d4866ac08d84ee4e54d429c3201e5df564c5031
Pedro Airton <pedroairton01@gmail.com>
2026-05-28 22:58:34 -0300

    Form de filme finalizado HTML, falta lógica de envio

--- src/app/features/admin/movie-detail/movie-detail.ts
+++ src/app/features/admin/movie-detail/movie-detail.ts
@@ -8,7 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -23,12 +23,28 @@ export class MovieDetail {
   movie = signal<Movie | null>(null)
   private adminService = inject(AdminService)
   private toastr = inject(ToastrService)
-  movieForm = {}
+
+  private fb = inject(FormBuilder)
+  movieForm: FormGroup
+
 
   constructor(private route: ActivatedRoute) {
     this.movieId = this.route.snapshot.params['id'];
     this.isEditMode.set(!!this.movieId);
     console.log(this.movieId);
+
+    this.movieForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+      synopsis: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+      duration: ['', [Validators.required, Validators.min(5), Validators.max(600)]],
+      age_rating: ['', [Validators.required]],
+      original_title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+      director: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+      distributor: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+      country_of_origin: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]],
+      release_date: ['', [Validators.required]],
+      status: ['', [Validators.required]],
+    })
   }
 
   ngOnInit() {
@@ -47,5 +63,15 @@ export class MovieDetail {
         this.toastr.error('Erro ao carregar o filme');
       }
     })
+  }
+  onFileSelected(e: any){
+    console.log(e)
+  }
+  removeImage(){
+    console.log(this.movie());
+    // this.movie.set({
+    //   ...this.movie(),
+    //   image_url: null
+    // })
   }
 }
