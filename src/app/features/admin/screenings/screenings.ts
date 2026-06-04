bb320d68292a97f594c6c17323ab4f91616851e8
pedroairtonz <pedroairton@zaite.com.br>
2026-06-04 16:50:14 -0300

    Página de sessões criada

--- src/app/features/admin/screenings/screenings.ts
+++ src/app/features/admin/screenings/screenings.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-screenings',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './screenings.html',
   styleUrl: './screenings.scss',
 })
