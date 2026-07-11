cb0ae55fe6e18845af33324d899d29a3eb4db872
Pedro Airton <pedroairton01@gmail.com>
2026-07-11 16:00:19 -0300

    Página historico concluida

--- src/app/features/user/history/history.ts
+++ src/app/features/user/history/history.ts
@@ -1,24 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Ticket } from '../../../core/models/history.model';
 import { ApiService } from '../../../core/services/api.service';
 import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../../../environments/environment';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './history.html',
   styleUrl: './history.scss',
 })
 export class History {
-  tickets: Ticket[] = [];
+  // tickets: Ticket[] = [];
+  tickets = signal<Ticket[]>([])
   private apiService = inject(ApiService)
   private toastr = inject(ToastrService)
+  storageUrl = environment.storageUrl
 
   ngOnInit() {
     this.apiService.getTickets().subscribe({
       next: (res: any) => {
-        this.tickets = res.data;
-        console.log(this.tickets);
+        this.tickets.set(res.data);
+        console.log(this.tickets());
       },
       error: (err) => {
         this.toastr.error(err.error.message);
